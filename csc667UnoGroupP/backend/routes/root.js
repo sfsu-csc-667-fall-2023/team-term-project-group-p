ff9debac780d869987afb59e543497471ae75af4
Leiwo94 <kwoods1@sfsu.edu>
2023-12-10 12:03:10 -0800

    finished package

--- csc667UnoGroupP/backend/routes/root.js
+++ csc667UnoGroupP/backend/routes/root.js
@@ -1,13 +1,10 @@
 const express = require('express');
 
-
-
 const router = express.Router();
 
 router.get("/", (_req, res) => {
     res.send("Hello world form inside a route!");
 });
-
 
 router.get("/:id", (_req, res) =>{
     res.send(`Other route ${_req.params.id}`);
