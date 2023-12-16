23eaf52d15e1159fd1cb7e06d7ae1b4b0d32d830
Himal Shrestha <kshrestha3@sfsu.com>
2023-12-15 17:18:13 -0800

    made successful connection to mysql db

---
+++ csc667UnoGroupP/backend/routes/login.js
@@ -0,0 +1,72 @@
+import express from 'express';
+const loginRouter = express.Router();
+//import process from 'process';
+// const frontend = process.chdir('../frontend');
+// const frontend_dir = process.cwd();
+import path from path;
+import mysql from 'mysql2';
+import bcrypt from 'bcrypt';
+
+
+const database = mysql.createConnection({
+    host: 'localhost',
+    database: 'csc667uno',
+    user: 'root',
+    password: 'abcd',
+    
+});
+// this will render the registration page
+loginRouter.get("/", (req, res) => {
+    res.render("landing");
+    
+});
+
+// registers user after form submission
+loginRouter.post("/", (req, res) => {
+    const { username, password} = req.body;
+    // res.status(200).send('Registration post page');
+    database.query('SELECT * FROM Users WHERE username = ?', [username], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error querying the database: ' });
+        }
+
+        if (results.length === 0) {
+            return res.status(401).json({ error: 'Username does not exist. Please register' });
+        }
+
+        const user = results[0];
+
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error comparing passwords: ' });
+
+            }
+            if (!result) {
+                return res.status(401).json({ error: 'Wrong password' });
+            }
+            req.session.username = user.username;
+             console.log(user.username);
+            req.session.user_type = user.user_type;
+             console.log("user_type", req.session.user_type);
+            req.session.user_id = user.users_id;
+            console.log("user id", req.session.user_id );
+
+            res.redirect('/'); // after successsful login, renders user to homepage 
+
+        });
+    });
+    
+});
+
+loginRouter.get("/logout", (req, res) => {
+    if (req.session.username !== null){
+        req.session.username = null;
+        req.session.user_type = null;
+        req.session.user_id = null;
+        
+    }
+    console.log(req.session.username);
+    return res.redirect('/login');
+
+})
+export { loginRouter };
--- csc667UnoGroupP/backend/server.js
+++ csc667UnoGroupP/backend/server.js
@@ -1,24 +1,80 @@
-const express = require('express');
+// const express = require('express');
 
-const creatError = require("http-errors");
+// const creatError = require("http-errors");
 
-const { requestTime } = require("./middleware/request-time");
+// const { requestTime } = require("./middleware/request-time");
+// const app = express();
+
+// const PORT = process.env.PORT || 3000;
+
+
+// app.use(requestTime);
+
+// const rootRoutes = require("./routes/root");
+
+
+// app.use("/", rootRoutes);
+
+// app.use((_req, res, next) => {
+//     next(createError(404));
+// });
+
+// app.listen(PORT, () =>{
+//     console.log(`Server started on port ${PORT}`);
+// })
+
+const path = require("path");
+require("dotenv").config({path:'./.env'});
+
+const mysql = require('mysql2');
+
+// import ejs from 'ejs';
+
+const express = require("express");
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-
-app.use(requestTime);
-
-const rootRoutes = require("./routes/root");
-
-
-app.use("/", rootRoutes);
-
-app.use((_req, res, next) => {
-    next(createError(404));
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT
 });
 
-app.listen(PORT, () =>{
-    console.log(`Server started on port ${PORT}`);
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'P@$sw0rd',
+//     database: 'CSC667TeamPuno',
+//     port: process.env.PORT || 3306
+// });
+
+db.connect((err) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("Database connected...!")
+    }
 })
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "backend", "static")));
+
+app.get("/", (request, response) => {
+    response.render("landing");
+});
+app.listen(PORT, () => {
+console.log(`Server started on port ${PORT}`);
+});
